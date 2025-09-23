4741f3afb0a3e9a7cb3693bcfd3adbfadc2862e6
AnnaThario <akmthario@gmail.com>
2025-09-23 11:38:05 -0700

    Added in code to remove duplicate values (by combining the counts).

--- DataCheck.R
+++ DataCheck.R
@@ -25,5 +25,30 @@ dupes<-data_sub[which(duplicated(data_sub)),]
 #filtering data in the big dataset that matches our duplicates
 filtered_df <- semi_join(x = data, y = dupes)
 
-#Writing to a .csv
+#Writing to a .csv to assess
 write.csv(filtered_df, file = "duplicates_PSSB2015-2025.csv")
+
+#-----------------------------------------------
+#For 2025, we decided that the duplicate values shouldn't impact the BIBI score.Thus, we decided to combine the counts for the duplicates. 
+
+#Combining the counts of all entries based on the same criteria we used to determine the duplicates
+
+#Pulling out all of the info associated with each unique combo of sample id, life stage, and taxa
+metadata <- data %>%
+  distinct(Sample_ID, Result_Taxon_Name, Result_Taxon_Life_Stage, .keep_all = TRUE) %>%
+  select(-Result_Value)
+
+#Adding together the counts for all entries with the same unique combo, then joining the other columns back in
+newcounts <- summarize(
+  data,
+  Result_Value = sum(Result_Value),
+  .by = c(Sample_ID, Result_Taxon_Name, Result_Taxon_Life_Stage)
+) %>%
+  left_join(metadata, by = c("Sample_ID", "Result_Taxon_Name", "Result_Taxon_Life_Stage"))
+
+#Removing duplicates based on the previous criteria, keeping all of the columns
+#Not necessary, but a good final check
+final_df <- distinct(newcounts, Sample_ID, Result_Taxon_Name, Result_Taxon_Life_Stage, .keep_all = TRUE)
+
+#Saving data to new .csv
+write.csv(final_df, file = "EIM_data_2015-2025.csv")
